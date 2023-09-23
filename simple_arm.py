0b6f1c41ddec3a607b07ed72500eea3c8ef3fdc2
Layth Aljorani <ladcz@mst.edu>
2023-09-23 16:05:08 -0500

    init

--- simple_arm.py
+++ simple_arm.py
@@ -7,7 +7,7 @@ import math
 p.connect(p.GUI) # p.DIRECT for non-graphical version
 p.resetSimulation() # reset
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) # to import assets
-p.setGravity(0,0,-10) # x,y,z gravity
+p.setGravity(0,0,-9.807) # x,y,z gravity
 p.setRealTimeSimulation(0) # r-t simulation
 
 
@@ -16,13 +16,32 @@ p.loadURDF(fileName="plane.urdf", basePosition=[0,0,0], baseOrientation=[0,0,0,1
 object = p.loadURDF(fileName="franka_panda/panda.urdf", basePosition=[0,0,0], baseOrientation=[0,0,0,1], useFixedBase=True)
 
 # for all joints of this object, print info
+# but what do the values mean?
+# https://github.com/bulletphysics/bullet3/blob/master/examples/pybullet/notebooks/HelloPyBullet.ipynb
+# index: int
+# name: str
+# type: int
+# gIndex: int
+# uIndex: int
+# flags: int
+# damping: float
+# friction: float
+# lowerLimit: float
+# upperLimit: float
+# maxForce: float
+# maxVelocity: float
+# linkName: str
+# axis: tuple
+# parentFramePosition: tuple
+# parentFrameOrientation: tuple
+# parentIndex: int
 for i in range(p.getNumJoints(object)):
     print(p.getJointInfo(object, i))
 
 for step in range(300):
     pos, ori = p.getBasePositionAndOrientation(object)
-    # to create a static camera
-    p.resetDebugVisualizerCamera(cameraDistance=3, cameraYaw=3, cameraPitch=-70, cameraTargetPosition=pos)
+    # to create a static camera instead of free movement... why, lol?
+    # p.resetDebugVisualizerCamera(cameraDistance=3, cameraYaw=3, cameraPitch=-70, cameraTargetPosition=pos)
     p.stepSimulation()
     time.sleep(.01)
 

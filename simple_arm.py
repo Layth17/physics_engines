36be8ad5f77b58483c8198b5beea1adad77d4707
Layth Aljorani <46543673+Layth17@users.noreply.github.com>
2023-09-23 14:55:13 -0500

    Add files via upload

---
+++ simple_arm.py
@@ -0,0 +1,28 @@
+import pybullet as p
+import pybullet_data
+import time
+import math
+
+# Set up; create empty env
+p.connect(p.GUI) # p.DIRECT for non-graphical version
+p.resetSimulation() # reset
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) # to import assets
+p.setGravity(0,0,-10) # x,y,z gravity
+p.setRealTimeSimulation(0) # r-t simulation
+
+
+# load; URDF: Universal Robot Description File
+p.loadURDF(fileName="plane.urdf", basePosition=[0,0,0], baseOrientation=[0,0,0,1])
+object = p.loadURDF(fileName="franka_panda/panda.urdf", basePosition=[0,0,0], baseOrientation=[0,0,0,1], useFixedBase=True)
+
+for i in range(p.getNumJoints(object)):
+    print(p.getJointInfo(object, i))
+
+for step in range(300):
+    pos, _ = p.getBasePositionAndOrientation(object)
+    p.resetDebugVisualizerCamera(cameraDistance=3, cameraYaw=3, cameraPitch=-70, cameraTargetPosition=pos)
+    p.stepSimulation()
+    time.sleep(.01)
+
+if __name__ == '__main__':
+    pass
